4937cb56df1f864d221e64b68837754be9c15be8
WANGZHE <1103270775@qq.com>
2020-12-20 19:41:43 +0800

    日期格式化

--- src/main/java/com/ctgu/airticket/entity/TTicketorder.java
+++ src/main/java/com/ctgu/airticket/entity/TTicketorder.java
@@ -1,5 +1,6 @@
 package com.ctgu.airticket.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import javax.persistence.*;
 
@@ -23,7 +24,8 @@ public class TTicketorder implements Serializable {
     private Integer orderid;
     
     private String orderno;
-    
+    //日期格式化
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date ordertime;
     
     private Integer price;
