20a05de2d0ba96ab4fbfd50e1caa7c86b6644093
zyw_glb <1819775269@qq.com>
2020-12-20 16:57:15 +0800

    增加弹框表单模板

--- src/main/java/com/ctgu/airticket/controller/TFlightHandler.java
+++ src/main/java/com/ctgu/airticket/controller/TFlightHandler.java
@@ -1,5 +1,6 @@
 package com.ctgu.airticket.controller;
 
+import com.ctgu.airticket.entity.CommonResult;
 import com.ctgu.airticket.entity.TFlight;
 import com.ctgu.airticket.entity.TUser;
 import com.ctgu.airticket.repository.TFlightRepository;
@@ -7,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Null;
+import javax.websocket.server.PathParam;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -23,41 +28,43 @@ public class TFlightHandler {
     TFlightRepository tFlightRepository;
     //增加一条航班信息
     @PostMapping("/save")
-    public String save(@RequestBody TFlight tFlight){
+    public CommonResult<Null> save(@RequestBody TFlight tFlight){
         TFlight save = tFlightRepository.save(tFlight);
         if (save!=null){
-            return "success";
+            return new CommonResult<>(200, "success", 1, null);
         }else {
-            return "error";
+            return new CommonResult<>(200, "error", 1, null);
         }
     }
     @DeleteMapping("/deleteById")
-    public String deleteById(@RequestBody TFlight tFlight){
+    public CommonResult<Null> deleteById(@RequestBody TFlight tFlight){
         tFlightRepository.deleteById(tFlight.getFid());
-        return "success";
+        return new CommonResult<>(200, "success", 1, null);
     }
     @PutMapping("/update")
-    public String update(@RequestBody TFlight tFlight){
+    public CommonResult<Null> update(@RequestBody TFlight tFlight){
         TFlight result = tFlightRepository.save(tFlight);
         if(result!= null){
-            return "success";
+            return new CommonResult<>(200, "success", 1, null);
         }else{
-            return "error";
+            return new CommonResult<>(200, "error", 1, null);
         }
     }
     @GetMapping("/findById")
-    public TFlight findById(@RequestBody TFlight tFlight){
+    public CommonResult<TFlight> findById(@RequestBody TFlight tFlight){
         Optional<TFlight> result = tFlightRepository.findById(tFlight.getFid());
         if (result!=null) {
-            return result.get();
+            return new CommonResult<>(200, "success", 1, result.get());
         }else {
-            return null;
+            return new CommonResult<>(200, "error", 1, null);
         }
     }
-    @GetMapping("/findAll/{page}/{size}")
-    public Page<TFlight> findAll(@PathVariable("page") Integer page, @PathVariable("size") Integer size){
+    @GetMapping("/findAll")
+    public CommonResult<List<TFlight>> findAll(@PathParam("page") Integer page, @PathParam("size") Integer size){
         PageRequest request = PageRequest.of(page,size);
-        return tFlightRepository.findAll(request);
+        List<TFlight> content = tFlightRepository.findAll(request).getContent();
+        int count=(int)tFlightRepository.count();
+        return new CommonResult<>(200, "航班信息列表", count, content);
     }
 
 }
